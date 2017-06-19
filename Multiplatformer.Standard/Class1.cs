c55275c31a9d9b0b9d1f4ebf4af29b9447cef452
budougumi0617 <budougumi0617@gmail.com>
2017-06-19 10:27:34 +0900

    :+1: Use .NET Standard 1.6 library.

---
+++ Multiplatformer.Standard/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multiplatformer.Standard
+{
+    public class Class1
+    {
+        public static void SayHello()
+        {
+            System.Console.WriteLine("Hello .NET Standard.");
+        }
+    }
+}
